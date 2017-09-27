0ab504e9caeb130e6d4305b535b141a49b8f9d7f
justinstoner <justin@studiosuite.com>
2017-09-26 19:52:00 -0700

    serve static files from /public

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/users', users);
 app.use('/api/crypto', crypto);
 app.use('/coin-icon', express.static(path.join(__dirname, 'node_modules/cryptocoins-icons/SVG')))
